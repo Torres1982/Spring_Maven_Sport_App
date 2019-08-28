cd569c2097db27823184766b1333d272bb411777
Artur Torres <artur.sukiennik@wipro.com>
2019-08-28 10:08:09 +0100

    First commit

--- src/com/torres/config/SportAppConfig.java
+++ src/com/torres/config/SportAppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
  
 @Configuration
@@ -18,4 +19,8 @@ public class SportAppConfig {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+	
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
 }
--- src/com/torres/controller/MainController.java
+++ src/com/torres/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.torres.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedHashMap;
 
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.torres.model.Footballer;
@@ -34,5 +37,37 @@ public class MainController {
 		model.addAttribute("footballer", footballer);
 		
 		return "registration_form";
+	}
+	
+	@ModelAttribute("positionSelection")
+	public ArrayList<String> positionSelection() {
+		ArrayList<String> positionSelection = new ArrayList<String>();
+		positionSelection.add("Goalkeeper");
+		positionSelection.add("Defender");
+		positionSelection.add("Midfielder");
+		positionSelection.add("Attacker");
+		
+		return positionSelection;
+	}
+		
+	@ModelAttribute("strengthSelection")
+	public LinkedHashMap<String, String> strengthOptions() {
+		LinkedHashMap<String, String> strengthOptions;
+		final ArrayList<String> strengthsArray = new ArrayList<String>() {
+			private static final long serialVersionUID = 1L; {
+			add("Agility"); add("Balance and Coordination"); add("Ball Possession"); add("Catching Ball"); add("Crossing");
+			add("Dribbling"); add("Finishing"); add("Free Kicks"); add("Heading"); add("Jump"); add("Long Throw");
+			add("Long Passing"); add("Passing"); add("Penalties Saving"); add("Power and Strength"); add("Shot Stopping");
+			add("Speed"); add("Stamina"); add("Tackling");
+		}};
+		
+		// Populate Strength Options
+		strengthOptions = new LinkedHashMap<String, String>();
+		strengthOptions.put("", "Select Main Strength:");
+		
+		for (String strength : strengthsArray) {
+			strengthOptions.put(strength, strength);
+		}
+		return strengthOptions;
 	}
 }
