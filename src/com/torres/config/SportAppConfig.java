adc33d58ca439477b0792785b61a82f976e1649d
Artur Torres <artur.sukiennik@wipro.com>
2019-08-28 10:52:22 +0100

    TEST

--- src/com/torres/config/SportAppConfig.java
+++ src/com/torres/config/SportAppConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
  
 @Configuration
@@ -19,8 +18,4 @@ public class SportAppConfig {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
-	
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-    }
 }
