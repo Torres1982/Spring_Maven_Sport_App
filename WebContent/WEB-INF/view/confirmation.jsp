d9e29fe38c1ca4ad89cfbb8e7400998c9340fcc7
Artur Torres <artur.sukiennik@wipro.com>
2019-08-28 12:35:35 +0100

    Processing the Registration form added

---
+++ WebContent/WEB-INF/view/confirmation.jsp
@@ -0,0 +1,50 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+		<title>Confirmation Page</title>
+		<!-- CSS References -->
+		<link href="<c:url value="/resources/css/main.css" />" rel="stylesheet">
+		<link href="<c:url value="/resources/css/vendor/bootstrap.min.css" />" rel="stylesheet">
+	</head>
+
+	<body>
+		<div class="container page_container">
+			<c:choose>
+				<c:when test="${not empty footballer}">
+					<c:if test="${not empty message}">
+						<h4>${message}</h4>
+					</c:if>
+					
+					<p>${footballer.firstName} ${footballer.lastName} - ${footballer.club} (${footballer.country})</p>
+					<p>Date of Birth: ${footballer.dob}</p>
+					<p>Main Strength: ${footballer.strength}</p>
+					<p>Main Position: ${footballer.position}</p>			
+					<p>Category: ${footballer.category}</p>
+					<p>Overall Rating: ${footballer.rating}</p>
+				</c:when>
+				
+				<c:otherwise>
+					<c:if test="${not empty deleteMessage}">
+						<h4>${deleteMessage}</h4>
+					</c:if>
+				</c:otherwise>
+			</c:choose>
+			
+			<a href="home" class="link-text">Home Page</a>
+			
+			<div id="footer" class="text-center">
+				Artur Sukiennik | &copy;opyright | Wipro Limited | 2019
+			</div>
+		</div>
+	</body>
+	
+	<!-- Java Script References -->
+   	<script src="<c:url value="/resources/js/vendor/jquery-3.4.1.js" />"></script>
+   	<script src="<c:url value="/resources/js/vendor/popper.min.js" />"></script>
+   	<script src="<c:url value="/resources/js/vendor/bootstrap.min.js" />"></script>
+   	<script src="<c:url value="/resources/js/main.js" />"></script>
+</html>
--- src/com/torres/controller/MainController.java
+++ src/com/torres/controller/MainController.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedHashMap;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.torres.model.Footballer;
@@ -28,6 +32,7 @@ public class MainController {
 	@RequestMapping({"/", "/home"})
 	public String showIndexPage(Model model) {
 		model.addAttribute("todayDate", new Date());
+		
 		return "home";
 	}
 	
@@ -37,6 +42,29 @@ public class MainController {
 		model.addAttribute("footballer", footballer);
 		
 		return "registration_form";
+	}
+	
+	@PostMapping("/processRegistrationForm")
+	public String processRegistrationForm(@Valid @ModelAttribute("footballer") Footballer footballer, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			System.out.println("Some Required Fields are Left Blank!");
+			System.out.println("Binding Result: " + result);
+			
+			return "registration_form";
+		} else {
+			if (footballer.getId() == 0) {
+				model.addAttribute("message", "New Footballer Registered:");
+			} else {
+				model.addAttribute("message", "Existing Footballer Updated:");
+			}
+			
+			// TODO : Add below line when DB will be applied
+			//footballerService.createFootballer(footballer);					
+			System.out.println("Footballer Saved Successfully! Id: " + footballer.getId() + " " + footballer.getFirstName() + " " + footballer.getLastName());
+
+			return "confirmation";
+			//return "redirect:/main/showConfirmation";
+		}
 	}
 	
 	@ModelAttribute("positionSelection")
